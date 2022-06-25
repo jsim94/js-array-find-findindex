a11164c2ff75a93a0e496d0c689709d9614a09bd
jsim94 <j.sime94@gmail.com>
2022-06-24 23:42:21 -0400

    wrote code to satisfy reqs

--- find-findindex.js
+++ find-findindex.js
@@ -11,8 +11,11 @@ findUserByUsername(users, 'mlewis') // {username: 'mlewis'}
 findUserByUsername(users, 'taco') // undefined
 */
 
-
-function findUserByUsername(usersArray, username) {}
+function findUserByUsername(usersArray, username) {
+  return usersArray.find((val) => {
+    return val.username === username;
+  });
+}
 
 /*
 Write a function called `removeUser` which accepts an array of objects, each with a key of username, and a string. The function should remove the object from the array. If the object is not found, return undefined. 
@@ -27,4 +30,11 @@ removeUser(users, 'akagen') // {username: 'akagen'}
 removeUser(users, 'akagen') // undefined
 */
 
-function removeUser(usersArray, username) {}
+function removeUser(usersArray, username) {
+  let userIndex = usersArray.findIndex((val) => {
+    return val.username === username;
+  });
+  if (userIndex === -1) return;
+
+  return usersArray.splice(userIndex, 1)[0];
+}
